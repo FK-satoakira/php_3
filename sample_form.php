315e138bc53b9905869ac320232a2207ddbbd46f
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-11 16:17:49 +0900

    Add files via upload

---
+++ sample_form.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_GET["name"];
+$comment = $_GET["comment"];
+echo $name."<br>".$comment;
+// echo $comment;
+
+
+// echo <<< _FORM_
+// <!doctype html>
+// <html>
+// <head>
+// <meta charset="utf-8">
+// <title>確認フォームPHP3</title>
+// </head>
+// <body>
+//    <p>
+//        ■お名前<br>
+// 	  $name
+//    </p>
+//    <p>
+//        ■コメント<br>
+//        $comment
+//    </p>
+// </body>
+// </html>
+// _FORM_;
+// ?>
+
+
+<!-- HTMLファイルとphpファイルが対になってないとできない -->
+<!-- ヒアドキュメントでなくても平気だった -->
